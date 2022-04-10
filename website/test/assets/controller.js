1dc90ae06b4fe4460af8123124445abd335b60a3
Ernesto Farina <ernestofarina702@gmail.com>
2022-04-10 12:37:13 -0500

    desplegate the submenu of client

--- website/test/assets/controller.js
+++ website/test/assets/controller.js
@@ -12,3 +12,23 @@ desplegate.onclick = function(){
     }
     open = !open;
 }
+
+let sale = document.getElementById("saleBTN");
+let subSale = document.getElementById("sub-venta");
+
+sale.onclick = function(){
+    sale.setAttribute("style", "background: linear-gradient(to left, #01da42, #00FF4C);");
+    sale.children.item(0).setAttribute("style", "content: url(\"./assets/images/icons8_cash_register_euro_100px@2x.png\");");
+    sale.children.item(1).setAttribute("style", "color: white");
+    sale.children.item(2).setAttribute("style", "content: url(\"./assets/images/icon-right-white.png\"); transform: rotate(90deg); transition: transform .5s");
+
+    subSale.setAttribute("style", "height: 100px; transition: all 1s");
+}
+
+let client = document.getElementById("clientBTN");
+client.onclick = function(){
+    client.setAttribute("style", "background: linear-gradient(to left, #01da42, #00FF4C);");
+    // sale.children.item(0).setAttribute("style", "content: url(\"./assets/images/icons8_cash_register_euro_100px@2x.png\");");
+    client.children.item(1).setAttribute("style", "color: white");
+    client.children.item(2).setAttribute("style", "content: url(\"./assets/images/icon-right-white.png\"); transform: rotate(90deg); transition: transform .5s");
+}
