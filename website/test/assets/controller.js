c91007fe383d8d9be8ff9cf10518ccca8638d024
Ernesto Farina <ernestofarina702@gmail.com>
2022-04-12 14:53:26 -0500

    creating view new sale

--- website/test/assets/controller.js
+++ website/test/assets/controller.js
@@ -15,17 +15,16 @@ desplegate.onclick = function(){
 
 let sale = document.getElementById("saleBTN");
 let subSale = document.getElementById("sub-venta");
+let client = document.getElementById("clientBTN");
 
 sale.onclick = function(){
-    sale.setAttribute("style", "background: linear-gradient(to left, #01da42, #00FF4C);");
+    sale.setAttribute("style", "background: linear-gradient(to left, #01da42, #00FF4C); box-shadow: 0px 2px 5px rgb(122, 122, 122);");
     sale.children.item(0).setAttribute("style", "content: url(\"./assets/images/icons8_cash_register_euro_100px@2x.png\");");
     sale.children.item(1).setAttribute("style", "color: white");
     sale.children.item(2).setAttribute("style", "content: url(\"./assets/images/icon-right-white.png\"); transform: rotate(90deg); transition: transform .5s");
 
     subSale.setAttribute("style", "height: 100px; transition: all 1s");
 }
-
-let client = document.getElementById("clientBTN");
 client.onclick = function(){
     client.setAttribute("style", "background: linear-gradient(to left, #01da42, #00FF4C);");
     // sale.children.item(0).setAttribute("style", "content: url(\"./assets/images/icons8_cash_register_euro_100px@2x.png\");");
